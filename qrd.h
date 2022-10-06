2382dcaf9c2d55f6e8ef61ae157d311ff4e5341b
Kosta-Gecov <kostagecov@gmail.com>
2022-10-06 17:04:50 +0200

    # Two implementations tested: iterative and "successive". Seems to work well
    Signed-off-by: Kosta-Gecov <kostagecov@gmail.com>

--- qrd.h
+++ qrd.h
@@ -1,15 +1,20 @@
 #ifndef _QRD_
 #define _QRD_
 
-#define TAM 4
+#define TAM 5
 
 #include <ap_fixed.h>
 #include <vector>
 
-typedef ap_fixed<16, 7, AP_RND> data_t; // 16 bits fixed point data, 7 for integer value and 3 for decimals
+
+// The more bits it has, more shifts can be performed later, so the approximation to 0 will be more precise
+// For bigger matrices, we need bigger data formats to be able to calculate the right result
+typedef ap_fixed<24, 10, AP_RND> data_t; // 16 bits fixed point data, 7 for integer value and 3 for decimals
 
 typedef std::vector<std::vector<data_t>> Matrix;
 
-void rot_givens(/*const Matrix &A, const Matrix &R, */Matrix &res, bool sign);
+void rot_givens(Matrix &A);
+
+void rot_givens_succ(Matrix &A, data_t X, data_t Y, bool &sign, int n_iter, int row_X, int row_Y, int col);
 
 #endif
