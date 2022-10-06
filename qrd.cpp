fbd5795026b6db00bf2424e2c0168d68ea17af26
Kosta-Gecov <kostagecov@gmail.com>
2022-10-06 17:04:50 +0200

    # Two implementations tested: iterative and "successive". Seems to work well
    Signed-off-by: Kosta-Gecov <kostagecov@gmail.com>

--- qrd.cpp
+++ qrd.cpp
@@ -1,36 +1,80 @@
 #include <iostream>
 #include "qrd.h"
 
-void rot_givens(/*const Matrix &A, const Matrix &R, */Matrix &res, bool sign){
+void rot_givens(Matrix &A){
 	data_t coord_X, coord_Y;
 	data_t coord_X_shif, coord_Y_shif;
 
-	int n_iter = 1;
-	
+	int n_iter = 15;
+	bool sign;
+
     // Accesing coordinates X and Y
 
-	// The access to elements X and Y is incorrect, got to fix it
      for(int j = 0; j < TAM-1; j++){ // Columns
 	    for(int i = TAM-1; i >= 0; i--){ // Rows
-            if(i > j){
+            if(i > j /*&& (A[i][j] != 0)*/){
 
-                coord_X = res[i-1][j];
-                coord_Y = res[i][j];
+                coord_X = A[i-1][j];
+                coord_Y = A[i][j];
 
-                coord_X_shif = coord_X >> n_iter;
-                coord_Y_shif = coord_Y >> n_iter;
+                // Vectorization (Coordinate Y = 0)
+                for(int k = 0; k < n_iter; k++){
+					if(coord_Y < 0)
+						sign = true;
+					else
+						sign = false;
 
-                if(sign){
-                	res[i-1][j] = coord_X + coord_Y_shif;
-                	res[i][j] = coord_Y - coord_X_shif;
-                }else{
-                	res[i-1][j] = coord_X - coord_Y_shif;
-                    res[i][j] = coord_Y + coord_X_shif;
+					coord_X_shif = coord_X >> k;
+					coord_Y_shif = coord_Y >> k;
+
+					if(sign){
+						 coord_X = coord_X - coord_Y_shif;
+						 coord_Y = coord_Y + coord_X_shif;
+					}else{
+						coord_X = coord_X + coord_Y_shif;
+						coord_Y = coord_Y - coord_X_shif;
+					}
                 }
+                A[i-1][j] = coord_X;
+                A[i][j] = coord_Y;
 
-                sign = !sign;
-                n_iter++;
+                coord_X = 0;
+                coord_Y = 0;
+                coord_X_shif = 0;
+                coord_Y_shif = 0;
             }
+
 	    }
     }
+}
+
+void rot_givens_succ(Matrix &A, data_t X, data_t Y, bool &sign, int n_iter, int row_X, int row_Y, int col){
+	data_t coord_X, coord_Y;
+	data_t coord_X_shif, coord_Y_shif;
+
+	coord_X = X;
+    coord_Y = Y;
+
+
+    // Vectorization (Coordinate Y = 0)
+    for(int i = 0; i < n_iter; i++){
+    	if(coord_Y < 0)
+    	    	sign = false;
+    	    else
+    	    	sign = true;
+
+		coord_X_shif = coord_X >> i;
+		coord_Y_shif = coord_Y >> i;
+
+		if(sign){
+			 coord_X = coord_X - coord_Y_shif;
+			 coord_Y = coord_Y + coord_X_shif;
+		}else{
+			coord_X = coord_X + coord_Y_shif;
+			coord_Y = coord_Y - coord_X_shif;
+		}
+    }
+
+    A[row_X][col] = coord_X;
+    A[row_Y][col] = coord_Y;
 }
--- qrd_tb.cpp
+++ qrd_tb.cpp
@@ -4,17 +4,20 @@
 
 int main(){
 
-	//typedef ap_fixed<16, 7, AP_RND> data_t; // 16 bits fixed point data, 7 for integer value and 3 for decimals
-	Matrix A = {{3, 2, -1, 4},
-			    {2, 1, 5, 7},
-			    {0, 5, 2, -6},
-			    {-1, 2, 1, 0}};
 
+	Matrix A = {{3, 2, -1, 4, 1},
+			    {2, 1, 5, 7, 3},
+			    {0, 5, 2, -6, 5},
+			    {-1, 2, 1, 0, 7},
+				{-9, 16, 8, -1, 4}};
+
+/*
 	Matrix R = {{1, 0, 0, 0},
 			    {0, 1, 0, 0},
 			    {0, 0, 1, 0},
 			    {0, 0, 0, 1}};
-
+*/
+/*
 	Matrix res(TAM, std::vector<data_t>(TAM, 0)); // Upper-Triangular Matrix R
 
 	for(int i = 0; i < TAM; i++){
@@ -22,19 +25,42 @@ int main(){
 			res[i][j] = A[i][j];
 		}
 	}
+*/
+
+    bool sign; //= false; //(A[TAM-1][0] >= 0);
+    static int n_iter = 15;
+
+    rot_givens(A);
 
 
-    bool sign = (A[TAM-1][0] >= 0);
+/*
+    // Primera columna
 
-    rot_givens(/*A, R, */res, sign);
+    rot_givens_succ(A, A[2][0], A[3][0], sign, n_iter, 2, 3, 0);
 
+    rot_givens_succ(A, A[0][0], A[1][0], sign, n_iter, 0, 1, 0);
+
+    rot_givens_succ(A, A[0][0], A[2][0], sign, n_iter, 0, 2, 0);
+
+    // Segunda columna
+
+    rot_givens_succ(A, A[2][1], A[3][1], sign, n_iter, 2, 3, 1);
+
+    rot_givens_succ(A, A[1][1], A[2][1], sign, n_iter, 1, 2, 1);
+
+    // Tercera columna
+
+    rot_givens_succ(A, A[2][2], A[3][2], sign, n_iter, 2, 3, 2);
+
+*/
 
     // Print result matrix
     for(int i = 0; i < TAM; i++){
 		for(int j = 0; j < TAM; j++){
-			std::cout << res[i][j] << " ";
+			std::cout << A[i][j] << "	|	";
 		}
 		std:: cout << std::endl;
 	}
+
     return 0;
 }
