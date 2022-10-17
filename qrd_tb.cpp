99ea7cc1e271b003a66c162ca440df5e544d23b4
Kosta-Gecov <kostagecov@gmail.com>
2022-10-17 14:34:03 +0200

    # Removed rot_givens_succ in main

--- qrd_tb.cpp
+++ qrd_tb.cpp
@@ -18,33 +18,11 @@ int main(){
 							{0, 5, 2, -6},
 							{-1, 2, 1, 0}
 						 };
-
-    bool sign;
-    static int n_iter = 15;
 */
 
     rot_givens(A);
 
-/*
-    // Primera columna
 
-    rot_givens_succ(A, A[2][0], A[3][0], sign, n_iter, 2, 3, 0);
-
-    rot_givens_succ(A, A[0][0], A[1][0], sign, n_iter, 0, 1, 0);
-
-    rot_givens_succ(A, A[0][0], A[2][0], sign, n_iter, 0, 2, 0);
-
-    // Segunda columna
-
-    rot_givens_succ(A, A[2][1], A[3][1], sign, n_iter, 2, 3, 1);
-
-    rot_givens_succ(A, A[1][1], A[2][1], sign, n_iter, 1, 2, 1);
-
-    // Tercera columna
-
-    rot_givens_succ(A, A[2][2], A[3][2], sign, n_iter, 2, 3, 2);
-
-*/
 
     // Print result matrix
     for(int i = 0; i < TAM; i++){
