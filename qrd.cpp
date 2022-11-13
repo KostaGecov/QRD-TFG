ec33b1013c83a27c47d6e5b6d088ccf1cc862ea6
Kosta-Gecov <kostagecov@gmail.com>
2022-11-13 21:42:22 +0100

    implementado rotación del resto de elementos de las filas

--- qrd.cpp
+++ qrd.cpp
@@ -1,55 +1,53 @@
 #include <iostream>
 #include "qrd.h"
 
-
 void rot_givens(data_t A[TAM][TAM]) {
     data_t coord_X, coord_Y;
-    data_t coord_X_shif, coord_Y_shif;
-
-    int n_iter = 15;
+    int n_iter = 30;
     bool sign;
 
-    // Accesing coordinates X and Y
+    // Accessing coordinates X and Y
     rot_columns_for:
-    for (int j = 0; j < TAM - 1; j++) { // Columns
+    for (int j = 0; j < TAM-1; j++) { // Columns
     	rot_rows_for:
-        for (int i = TAM - 1; i >= 0; i--) { // Rows
-            if ((i > j) && (A[i][j] != 0)) {
+		for (int i = TAM - 1; i > j; i--) { // Rows
+			coord_X = A[i - 1][j];
+			coord_Y = A[i][j];
+            if (coord_Y != 0) {
+        	// Vectorization (Coordinate Y = 0)
+				for (int k = 0; k < n_iter; k++) {
+					if (coord_Y < 0){
+						sign = true;
+					}else{
+						sign = false;
+					}
 
-                coord_X = A[i - 1][j];
-                coord_Y = A[i][j];
-
-                // Vectorization (Coordinate Y = 0)
-                for (int k = 0; k < n_iter; k++) {
-                    if (coord_Y < 0)
-                        sign = true;
-                    else
-                        sign = false;
-
-                    coord_X_shif = coord_X >> k;
-                    coord_Y_shif = coord_Y >> k;
-
-                    if (sign) {
-                        coord_X = coord_X - coord_Y_shif;
-                        coord_Y = coord_Y + coord_X_shif;
-                    } else {
-                        coord_X = coord_X + coord_Y_shif;
-                        coord_Y = coord_Y - coord_X_shif;
-                    }
-                }
-                A[i - 1][j] = coord_X;
-                A[i][j] = coord_Y;
-
-                coord_X = 0;
-                coord_Y = 0;
-                coord_X_shif = 0;
-                coord_Y_shif = 0;
+					if (sign) {
+						coord_X = coord_X - (coord_Y >> k);
+						coord_Y = coord_Y + (coord_X >> k);
+					} else {
+						coord_X = coord_X + (coord_Y >> k);
+						coord_Y = coord_Y - (coord_X >> k);
+					}
+					// Rotate the resting elements of the rows
+					for(int r = j+1; r < TAM; r++){
+						if (sign) {
+							A[i-1][r] = A[i-1][r] + (A[i][r] >> k);
+							A[i][r] = A[i][r] - (A[i-1][r] >> k);
+						} else {
+							A[i-1][r] = A[i-1][r] - (A[i][r] >> k);
+							A[i][r] = A[i][r] + (A[i-1][r] >> k);
+						}
+					}
+				}
             }
+            A[i - 1][j] = coord_X;
+            A[i][j] = coord_Y;
 
         }
     }
 }
-
+/*
 void rot_givens_succ(Matrix & A, data_t X, data_t Y, bool & sign, int n_iter, int row_X, int row_Y, int col) {
     data_t coord_X, coord_Y;
     data_t coord_X_shif, coord_Y_shif;
@@ -80,3 +78,4 @@ void rot_givens_succ(Matrix & A, data_t X, data_t Y, bool & sign, int n_iter, in
     A[row_Y][col] = coord_Y;
 
 }
+*/
--- qrd.h
+++ qrd.h
@@ -4,17 +4,17 @@
 #define TAM 5
 
 #include <ap_fixed.h>
-#include <vector>
+//#include <vector>
 
 
 // The more bits it has, more shifts can be performed later, so the approximation to 0 will be more precise
 // For bigger matrices, we need bigger data formats to be able to calculate the right result
 typedef ap_fixed<24, 10, AP_RND> data_t; // 24 bits fixed point data, 10 for integer value and 14 for decimals
 
-typedef std::vector<std::vector<data_t>> Matrix;
+//typedef std::vector<std::vector<data_t>> Matrix;
 
 void rot_givens(data_t A[TAM][TAM]);
 
-void rot_givens_succ(Matrix &A, data_t X, data_t Y, bool &sign, int n_iter, int row_X, int row_Y, int col);
+//void rot_givens_succ(Matrix &A, data_t X, data_t Y, bool &sign, int n_iter, int row_X, int row_Y, int col);*/
 
 #endif
