1f299ce7aa5eb80cfe19f08a78972401aa2d683b
KostaGecov <kostagecov@gmail.com>
2023-04-19 20:05:45 +0200

    # Renamed object rotators for TTQRT operation

--- qrd.cpp
+++ qrd.cpp
@@ -141,65 +141,62 @@ void krnl_givens_rotation(data_t A[TAM_TILED][TAM], data_t A_rot[TAM_TILED][TAM]
 		}
 	} else if(type_op == TTQRT){
 		// Rotators for GEQRT operation
-		Rotator rot1(0, 0, 0);
-		Rotator rot2(1, 1, 1);
-		Rotator rot3(2, 2, 2);
-		Rotator rot4(3, 3, 3);
-		Rotator rot5(4, 4, 4);
-		Rotator rot6(5, 5, 5);
+		Rotator Rot1_TT(0, 0, 0);
+		Rotator Rot2_TT(1, 1, 1);
+		Rotator Rot3_TT(2, 2, 2);
+		Rotator Rot4_TT(3, 3, 3);
+		Rotator Rot5_TT(4, 4, 4);
+		Rotator Rot6_TT(5, 5, 5);
 
-		Rotator rot7(0, 0, 1);
-		Rotator rot8(1, 1, 2);
-		Rotator rot9(2, 2, 3);
-		Rotator rot10(3, 3, 4);
-		Rotator rot11(4, 4, 5);
+		Rotator Rot7_TT(0, 0, 1);
+		Rotator Rot8_TT(1, 1, 2);
+		Rotator Rot9_TT(2, 2, 3);
+		Rotator Rot10_TT(3, 3, 4);
+		Rotator Rot11_TT(4, 4, 5);
 		
-		Rotator rot12(0, 0, 2);
-		Rotator rot13(1, 1, 3);
-		Rotator rot14(2, 2, 4);
-		Rotator rot15(3, 3, 5);
+		Rotator Rot12_TT(0, 0, 2);
+		Rotator Rot13_TT(1, 1, 3);
+		Rotator Rot14_TT(2, 2, 4);
+		Rotator Rot15_TT(3, 3, 5);
 		
-		Rotator rot16(0, 0, 3);
-		Rotator rot17(1, 1, 4);
-		Rotator rot18(2, 2, 5);
+		Rotator Rot16_TT(0, 0, 3);
+		Rotator Rot17_TT(1, 1, 4);
+		Rotator Rot18_TT(2, 2, 5);
 		
-		Rotator rot19(0, 0, 4);
-		Rotator rot20(1, 1, 5);
+		Rotator Rot19_TT(0, 0, 4);
+		Rotator Rot20_TT(1, 1, 5);
 		
-		Rotator rot21(0, 0, 5);
+		Rotator Rot21_TT(0, 0, 5);
 
-		// Read 6 input rows from first matrix
-		read_input_rows(A, rot1.row_x_in, rot1.row_y_in, rot2.row_x_in, rot2.row_y_in, rot3.row_x_in, rot3.row_y_in);
-		// Read 6 input rows from second matrix
-		read_input_rows(A, rot1.row_x_in, rot1.row_y_in, rot2.row_x_in, rot2.row_y_in, rot3.row_x_in, rot3.row_y_in);
+		// Read X coordinates rows from first matrix
+		read_input_rows(A, Rot1_TT.row_x_in, Rot2_TT.row_x_in, Rot3_TT.row_x_in, Rot4_TT.row_x_in, Rot5_TT.row_x_in, Rot6_TT.row_x_in);
+		// Read Y coordinates rows from second matrix
+		read_input_rows(A, Rot1_TT.row_y_in, Rot2_TT.row_y_in, Rot3_TT.row_y_in, Rot4_TT.row_y_in, Rot5_TT.row_y_in, Rot6_TT.row_y_in);
+
+		Rot1_TT.givens_rotation(Rot1_TT.row_x_in, Rot1_TT.row_y_in, Rot1_TT.row_x_out, Rot1_TT.row_y_out, Rot1_TT.col + col_offset);
+		Rot2_TT.givens_rotation(Rot2_TT.row_x_in, Rot2_TT.row_y_in, Rot2_TT.row_x_out, Rot2_TT.row_y_out, Rot2_TT.col + col_offset);
+		Rot3_TT.givens_rotation(Rot3_TT.row_x_in, Rot3_TT.row_y_in, Rot3_TT.row_x_out, Rot3_TT.row_y_out, Rot3_TT.col + col_offset);
+		Rot4_TT.givens_rotation(Rot4_TT.row_x_in, Rot4_TT.row_y_in, Rot4_TT.row_x_out, Rot4_TT.row_y_out, Rot4_TT.col + col_offset);
+		Rot5_TT.givens_rotation(Rot5_TT.row_x_in, Rot5_TT.row_y_in, Rot5_TT.row_x_out, Rot5_TT.row_y_out, Rot5_TT.col + col_offset);
+		Rot6_TT.givens_rotation(Rot6_TT.row_x_in, Rot6_TT.row_y_in, Rot6_TT.row_x_out, Rot6_TT.row_y_out, Rot6_TT.col + col_offset);
+		Rot7_TT.givens_rotation(Rot1_TT.row_x_out, Rot1_TT.row_y_out, Rot7_TT.row_x_out, Rot7_TT.row_y_out, Rot7_TT.col + col_offset);
+		Rot8_TT.givens_rotation(Rot2_TT.row_x_out, Rot2_TT.row_y_out, Rot8_TT.row_x_out, Rot8_TT.row_y_out, Rot8_TT.col + col_offset);
+		Rot9_TT.givens_rotation(Rot3_TT.row_x_out, Rot3_TT.row_y_out, Rot9_TT.row_x_out, Rot9_TT.row_y_out, Rot9_TT.col + col_offset);
+		Rot10_TT.givens_rotation(Rot4_TT.row_x_out, Rot4_TT.row_y_out, Rot10_TT.row_x_out, Rot10_TT.row_y_out, Rot10_TT.col + col_offset);
+		Rot11_TT.givens_rotation(Rot5_TT.row_x_out, Rot5_TT.row_y_out, Rot11_TT.row_x_out, Rot11_TT.row_y_out, Rot11_TT.col + col_offset);
+		Rot12_TT.givens_rotation(Rot7_TT.row_x_out, Rot7_TT.row_y_out, Rot12_TT.row_x_out, Rot12_TT.row_y_out, Rot12_TT.col + col_offset);
+		Rot13_TT.givens_rotation(Rot8_TT.row_x_out, Rot8_TT.row_y_out, Rot13_TT.row_x_out, Rot13_TT.row_y_out, Rot13_TT.col + col_offset);
+		Rot14_TT.givens_rotation(Rot9_TT.row_x_out, Rot9_TT.row_y_out, Rot14_TT.row_x_out, Rot14_TT.row_y_out, Rot14_TT.col + col_offset);
+		Rot15_TT.givens_rotation(Rot10_TT.row_x_out, Rot10_TT.row_y_out, Rot15_TT.row_x_out, Rot15_TT.row_y_out, Rot15_TT.col + col_offset);
+		Rot16_TT.givens_rotation(Rot12_TT.row_x_out, Rot12_TT.row_y_out, Rot16_TT.row_x_out, Rot16_TT.row_y_out, Rot16_TT.col + col_offset);
+		Rot17_TT.givens_rotation(Rot13_TT.row_x_out, Rot13_TT.row_y_out, Rot17_TT.row_x_out, Rot17_TT.row_y_out, Rot17_TT.col + col_offset);
+		Rot18_TT.givens_rotation(Rot14_TT.row_x_out, Rot14_TT.row_y_out, Rot18_TT.row_x_out, Rot18_TT.row_y_out, Rot18_TT.col + col_offset);
+		Rot19_TT.givens_rotation(Rot16_TT.row_x_out, Rot16_TT.row_y_out, Rot19_TT.row_x_out, Rot19_TT.row_y_out, Rot19_TT.col + col_offset);
+		Rot20_TT.givens_rotation(Rot17_TT.row_x_out, Rot17_TT.row_y_out, Rot20_TT.row_x_out, Rot20_TT.row_y_out, Rot20_TT.col + col_offset);
+		Rot21_TT.givens_rotation(Rot19_TT.row_x_out, Rot19_TT.row_y_out, Rot21_TT.row_x_out, Rot21_TT.row_y_out, Rot21_TT.col + col_offset);
 
 		/*
-			ToDo: Complete with correct rows
+			ToDo: Write 6 output rows to first matrix and Write 6 output rows to second matrix
 		*/
-		rot1.givens_rotation(rot1.row_x_in, rot1.row_y_in, rot1.row_x_out, rot1.row_y_out, rot1.col + col_offset);
-		rot2.givens_rotation(rot2.row_x_in, rot2.row_y_in, rot2.row_x_out, rot2.row_y_out, rot2.col + col_offset);
-		rot3.givens_rotation(rot3.row_x_in, rot3.row_y_in, rot3.row_x_out, rot3.row_y_out, rot3.col + col_offset);
-		rot4.givens_rotation(rot2.row_x_out, rot3.row_x_out, rot4.row_x_out, rot4.row_y_out, rot4.col + col_offset);
-		rot5.givens_rotation(rot2.row_y_out, rot3.row_y_out, rot5.row_x_out, rot5.row_y_out, rot5.col + col_offset);
-		rot6.givens_rotation(rot1.row_x_out, rot4.row_x_out, rot6.row_x_out, rot6.row_y_out, rot6.col + col_offset);
-		rot7.givens_rotation(rot1.row_y_out, rot5.row_x_out, rot7.row_x_out, rot7.row_y_out, rot7.col + col_offset);
-		rot8.givens_rotation(rot6.row_y_out, rot4.row_y_out, rot8.row_x_out, rot8.row_y_out, rot8.col + col_offset);
-		rot9.givens_rotation(rot7.row_y_out, rot5.row_y_out, rot9.row_x_out, rot9.row_y_out, rot9.col + col_offset);
-		rot10.givens_rotation(rot7.row_x_out, rot8.row_x_out, rot10.row_x_out, rot10.row_y_out, rot10.col + col_offset);
-		rot11.givens_rotation(rot9.row_x_out, rot8.row_y_out, rot11.row_x_out, rot11.row_y_out, rot11.col + col_offset);
-		rot12.givens_rotation(rot10.row_y_out, rot11.row_x_out, rot12.row_x_out, rot12.row_y_out, rot12.col + col_offset);
-		rot13.givens_rotation(rot11.row_y_out, rot9.row_y_out, rot13.row_x_out, rot13.row_y_out, rot13.col + col_offset);
-		rot14.givens_rotation(rot12.row_y_out, rot13.row_x_out, rot14.row_x_out, rot14.row_y_out, rot14.col + col_offset);
-		rot15.givens_rotation(rot14.row_y_out, rot13.row_y_out, rot15.row_x_out, rot15.row_y_out, rot15.col + col_offset);
-		rot16.givens_rotation(rot14.row_y_out, rot13.row_y_out, rot15.row_x_out, rot15.row_y_out, rot15.col + col_offset);
-		rot17.givens_rotation(rot14.row_y_out, rot13.row_y_out, rot15.row_x_out, rot15.row_y_out, rot15.col + col_offset);
-		rot18.givens_rotation(rot14.row_y_out, rot13.row_y_out, rot15.row_x_out, rot15.row_y_out, rot15.col + col_offset);
-		rot19.givens_rotation(rot14.row_y_out, rot13.row_y_out, rot15.row_x_out, rot15.row_y_out, rot15.col + col_offset);
-		rot20.givens_rotation(rot14.row_y_out, rot13.row_y_out, rot15.row_x_out, rot15.row_y_out, rot15.col + col_offset);
-		rot21.givens_rotation(rot14.row_y_out, rot13.row_y_out, rot15.row_x_out, rot15.row_y_out, rot15.col + col_offset);
-
-		// Write 6 output rows to first matrix
-		// Write 6 output rows to second matrix
-	
 	}
 }
